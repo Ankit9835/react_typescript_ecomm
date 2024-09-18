aa8353d39dc112bcf9a83f52d3a6cfed4e4be033
Ankit9835 <ankydchrismatic@gmail.com>
2024-09-18 21:36:26 +0530

    working on setting up pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import { Button } from './components/ui/button';
 import { useAppSelector } from './hooks';
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
 
-function App() {
-  const { name } = useAppSelector((state) => state.userState);
-  console.log(name);
-  return (
-    <div>
-      <h1 className='text-7xl font-bold '>App</h1>
-      <Button
-        variant='destructive'
-        size='lg'
-        onClick={() => console.log('it worked!!!')}
-      >
-        Click Me
-      </Button>
-    </div>
-  );
-}
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
 export default App;
