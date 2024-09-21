918fcc00fd8062c9e3b43c7d025b80eb1a2e86c9
Ankit9835 <ankydchrismatic@gmail.com>
2024-09-21 21:26:55 +0530

    working on layout file

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../components'
 
 const HomeLayout = () => {
   return (
-    <div>
-      HomeLayout
+    <>
+    <Header />
+    <nav>navbar</nav>
+    <div className='align-element py-20'>
+      <Outlet />
     </div>
+  </>
   )
 }
 
