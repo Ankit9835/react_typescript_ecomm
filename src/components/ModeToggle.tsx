2c3af4781d2c9272753bb579179e716562d86422
Ankit9835 <ankydchrismatic@gmail.com>
2024-09-22 21:54:02 +0530

    setting up navbar

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModeToggle = () => {
+  return (
+    <div>
+        ModeToggle
+    </div>
+  )
+}
+
+export default ModeToggle
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavLink = () => {
+  return (
+    <div>
+      Navlink
+    </div>
+  )
+}
+
+export default NavLink
